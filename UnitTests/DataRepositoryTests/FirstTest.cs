c981c52e2b75802a3fd629914bf754ebc576a511
StayCool <tobondini@gmail.com>
2013-07-12 18:26:39 +0300

    Created basic structure

---
+++ UnitTests/DataRepositoryTests/FirstTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests.DataRepositoryTests
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void Test1() {
+            Console.WriteLine("Hello");
+        }
+    }
+}
